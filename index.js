223fa29c0f975022b50c81873c540be4c39e61b2
Chimey Palyul <palyul.chimey@gmail.com>
2023-09-13 10:58:26 -0600

    obj[key] = value??

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const employee = {}
-employee.name = 'Sam'
-employee.streetAddress = "123 Waterloo Lane"
-function updateEmployeeWithKeyAndValue(obj,key,value){
-    return{
-        ...obj,
-        [key]: value,
-    };
-    }
-function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
-    obj[key] = value;
-    return obj;
-}
-function deleteFromEmployeeByKey(employee,key){
-    return{
-    ...employee,
-    [key]: undefined
-    }
-}
-function destructivelyDeleteFromEmployeeByKey(employee,key){
-    employee[key] = undefined;
-    return employee;
+const employee = {
+    name: "Sam",
+    streetAddress: "1 Apple St",
 }
 
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return{
+        ...employee,
+        [key]:value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key]=value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    return{
+        ...employee,
+        [key]: undefined,
+    }
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    employee[key]= undefined;
+    return employee;
+}
